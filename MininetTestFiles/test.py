6bbcb69eec6776d4382f061788b2bfcdf3720c60
Dascham <sydowtwenty@gmail.com>
2017-12-12 16:48:51 +0100

    stuff

--- MininetTestFiles/test.py
+++ MininetTestFiles/test.py
@@ -16,27 +16,32 @@ net.start()
 
 def Server():
 	h1 = net.get("h1")
-	result = h1.cmd("sudo python /etc/pythonprograms/pythonServer.py")
+	result = h1.cmd("sudo python ~/Desktop/pythonServer.py")
 	print(result)
 def Client():
 	h2 = net.get("h2")
-	result = h2.cmd("sudo python /etc/pythonprograms/pythonClient.py")
+	result = h2.cmd("sudo python ~/Desktop/pythonClient.py")
 	print(result)
 try:
 	thread1 = Thread(target=Server)
 	thread2 = Thread(target=Client)
 	print("starting thread 1")
 	thread1.start()
-	time.sleep(5)
+	time.sleep(3)
 	print("starting thread 2")
 	thread2.start()	
 except:
 	print("could not start threads")
 #let client and server run for a bit
 time.sleep(3)
-var = raw_input("Do thing")
-cli = CLI(net)
+net.interact()
+
+h1=net.get("h1")
+h1.terminate
+h2=net.get("h2")
+h2.terminate
+time.sleep(3)
 net.stop()
 
-#always do cleanup of mininet, easy
+#always do cleanup of mininet
 os.system("sudo mn -c")
--- PoxModule/stats.py
+++ PoxModule/stats.py
@@ -3,6 +3,11 @@ from pox.lib.recoco import Timer
 import pox.openflow.libopenflow_01 as of
 from pox.lib.util import dpidToStr
 
+from pox.openflow.libopenflow_01 import *
+from pox.lib.packet import *
+
+
+
 log = core.getLogger("Stats")
 
 tableActiveCount = {}
@@ -12,23 +17,43 @@ def _handle_ConnectionUp (event):
     allConnections.add(event.connection)
     print(event.connection)
 '''
+
+'''
 def _handle_TableStatsReceived(event):
-    print("Something even more")
     sw = 's%s'%event.dpid
     tableActiveCount[sw] = event.stats[0].active_count
     print("TableStatsReceived")
     print(tableActiveCount)
 
 def sendTableStatsRequest():
-    print("Something")
     for con in core.openflow.connections:
-        print("Something more")
         sr = of.ofp_stats_request()
         sr.type = of.OFPST_TABLE
         con.send(sr)
-        print("Send table stat message to Switch")
+        print("Sent table stat message to Switch")
+'''
+def HandleFlowStats(event):
+    ipaddress="10.0.0.1"
+    #match = of.ofp_match(nw_dst=ipaddress)
+
+    for flows in event.stats:
+        if flows.match.nw_dst == "10.0.0.1":
+            print("Printing stats")
+            print(flows.table_id)
+            for action in flows.actions:
+                print("Port %s" % action.port)
+
+def sendFlowStatsRequest():
+    for con in core.openflow.connections:
+        #sr = of.ofp_stats_request()
+        #sr.type = of.OFPST_FLOW
+        con.send(of.ofp_stats_request(body=of.ofp_flow_stats_request()))
+        print("Sent flow stats request message to Switch")
 
 def launch():
-    core.openflow.addListenerByName("TableStatsReceived", _handle_TableStatsReceived)
-    #core.openflow.addListenerByName("ConnectionUp", _handle_ConnectionUp)
-    Timer(5, sendTableStatsRequest, recurring=True)
+    #core.openflow.addListenerByName("TableStatsReceived", _handle_TableStatsReceived)
+    core.openflow.addListenerByName("FlowStatsReceived", HandleFlowStats)
+    Timer(5, sendFlowStatsRequest, recurring=True)
+
+def receivePanic():
+    open socket
